f4312bc93a78c6be90bf7cb21c888b0933ff284c
cardosohernandez <cardoso.hdz.miguel.cbtis16@gmail.com>
2016-04-15 12:20:35 -0500

    19subida

--- visor/engine1/script.js
+++ visor/engine1/script.js
@@ -18,4 +18,4 @@ function ws_blinds(c,b,a){var g=jQuery;var e=c.parts||3;var f=g("<div>");f.css({
 // Obfuscated by Javascript Obfuscator
 // http://javascript-source.com
 //***********************************************
-jQuery("#wowslider-container1").wowSlider({effect:"blinds",prev:"",next:"",duration:20*100,delay:22*100,width:960,height:360,autoPlay:true,playPause:true,stopOnHover:false,loop:false,bullets:true,caption:true,captionEffect:"slide",controls:true,onBeforeStep:0,images:0});
+jQuery("#wowslider-container1").wowSlider({effect:"blinds",prev:"",next:"",duration:20*100,delay:20*100,width:960,height:360,autoPlay:true,playPause:true,stopOnHover:false,loop:false,bullets:true,caption:false,captionEffect:"slide",controls:false,onBeforeStep:0,images:0});
